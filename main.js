4b0c98e555e73484689d06a295e28195ef257805
Matt <MVSucharski@gmail.com>
2018-07-02 17:17:18 -0400

    adding tour dates to landing

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 console.log('all hooked up');
-
+// side nav
 const navOpen = document.querySelector('#nav-button');
 const navClose = document.querySelector('#nav-button-close');
 let nav = document.querySelector('#nav');
-
-
 
 function openNavBar() {
     nav.style.display = 'block';
@@ -12,6 +10,9 @@ function openNavBar() {
   function closeNavBar() {
     nav.style.display = 'none';
   };
-
 navOpen.addEventListener('click', openNavBar);
 navClose.addEventListener('click', closeNavBar);
+
+
+
+// gallery
