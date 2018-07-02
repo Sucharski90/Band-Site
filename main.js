c772c68854d7d6183e554f9361c1a60b4b52b408
Matt <MVSucharski@gmail.com>
2018-07-02 17:00:35 -0400

    adding all files

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log('all hooked up');
+
+const navOpen = document.querySelector('#nav-button');
+const navClose = document.querySelector('#nav-button-close');
+let nav = document.querySelector('#nav');
+
+
+
+function openNavBar() {
+    nav.style.display = 'block';
+  };
+  function closeNavBar() {
+    nav.style.display = 'none';
+  };
+
+navOpen.addEventListener('click', openNavBar);
+navClose.addEventListener('click', closeNavBar);
