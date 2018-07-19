5d0be8291e1ec53c256bb1cf6edc9f97e894ab77
Matt <MVSucharski@gmail.com>
2018-07-19 15:27:24 -0400

    adding band members

--- main.js
+++ main.js
@@ -38,8 +38,22 @@ const members = [
     name: "Synyster Gates",
     instrument: "Lead Guitarist and Backing Vocalist",
     bio: "Brian Elwin Haner Jr. (born July 7, 1981), better known by his stage name Synyster Gates or simply Syn, is an American musician, best known for being the lead guitarist and backing vocalist of the band Avenged Sevenfold. He ranks No. 87 on Guitar World's 100 Greatest Guitarists of All Time. Gates was voted as Best Metal Guitarist in the World by Total Guitar in 2016 and once again in 2017"
+  },
+  {
+    name: "Brooks Wackerman",
+    instrument: "Drums",
+    bio: "On November 4, 2015, it was revealed that Wackerman was the new drummer for Avenged Sevenfold.[2] Brooks was working with the band one year prior to the official announcement, because the band wanted to fit in with the new drummer. Like the previous drummer Arin Ilejay, but unlike the rest of the band members, he currently does not have a stage name. His first full-length album with the band was their October 2016 release The Stage, which showcased his progressive style of drumming, often using fast double-bass kicks and blast beats."
+  },
+  {
+    name: "Zacky Vengeance",
+    instrument: "",
+    bio: ""
+  },
+  {
+    name: "Johnny Christ",
+    instrument: "",
+    bio: ""
   }
-
 ];
 
 // render info
@@ -53,6 +67,7 @@ const renderMembers = () => {
   bio.innerHTML = currentMember.bio;
 }
 renderMembers();
+
 // cycle members
 const nextMember = () => {
   index++;
